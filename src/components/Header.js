bf6a18af95ab66f2531c250d9a817c2ff6d7c870
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-12-08 19:25:54 +0530

    Header and Sidebar done also store step up

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { HAMBURGER_MENU, USER_ICON, YOUTUBE_LOGO } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+
+const Header = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          onClick={toggleMenuHandler}
+          className="h-8 cursor-pointer"
+          src={HAMBURGER_MENU}
+          alt="hamburger-menu"
+        />
+        <a href="/">
+          <img className="h-8 mx-2" src={YOUTUBE_LOGO} alt="youtube-logo" />
+        </a>
+      </div>
+      <div className="col-span-10 px-10">
+        <input
+          className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+          placeholder="Search"
+        />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img className="h-8" alt="user" src={USER_ICON} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const Sidebar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  if (!isMenuOpen) return null;
+
+  return (
+    <div className="p-5 shadow-lg w-40">
+      <ul>
+        <li>Home</li>
+        <li>Demo</li>
+        <li>Shorts</li>
+        <li>Videos</li>
+        <li>Live</li>
+      </ul>
+      <h1 className="font-bold pt-5">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+      <h1 className="font-bold pt-5">Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
